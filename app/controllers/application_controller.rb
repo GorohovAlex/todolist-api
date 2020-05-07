15e15774e3d87cc523fc5ba21a536e3e64c33746
GorohovAlex <gorochov.as@gmail.com>
2020-05-07 17:29:20 +0300

    Include JWTSessions and Serializer to Controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,24 @@
 class ApplicationController < ActionController::API
+  include JWTSessions::RailsAuthorization
+  rescue_from JWTSessions::Errors::Unauthorized, with: :not_authorized
+
+  private
+
+  def serialize_model(model, options = {})
+    options[:is_collection] = false
+    JSONAPI::Serializer.serialize(model, options)
+  end
+
+  def serialize_models(models, options = {})
+    options[:is_collection] = true
+    JSONAPI::Serializer.serialize(models, options)
+  end
+
+  def serialize_errors(errors)
+    JSONAPI::Serializer.serialize_errors(errors)
+  end
+
+  def not_authorized
+    render json: { error: 'Not authorized' }, status: :unauthorized
+  end
 end
