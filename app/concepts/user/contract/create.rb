57c3b611d7df6854108a3ab84de9800f8874ad40
GorohovAlex <gorohov.as@gmail.com>
2020-05-14 23:45:15 +0300

    Change valid params

--- app/concepts/user/contract/create.rb
+++ app/concepts/user/contract/create.rb
@@ -1,10 +1,15 @@
 class User::Contract::Create < Reform::Form
+  USERNAME_LENGTH = 3..50.freeze
+  PASSWORD_MIN_LENGTH = 6
+
   property :username
   property :password
   property :password_confirmation
 
-  validates :username, length: { minimum: 3 }
+  validates :username, length: USERNAME_LENGTH
   validates_uniqueness_of :username
+
+  validates :password, length: { minimum: PASSWORD_MIN_LENGTH }
 
   validates :password_confirmation, :password, presence: true
   validate :password_ok?
