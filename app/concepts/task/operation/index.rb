8fa316824a29e3819a42990c8f482677d51d9f42
GorohovAlex <gorohov.as@gmail.com>
2020-05-21 12:19:58 +0300

    Refactoring Task index

--- app/concepts/task/operation/index.rb
+++ app/concepts/task/operation/index.rb
@@ -1,18 +1,8 @@
 class Task::Operation::Index < Trailblazer::Operation
   step Policy::Pundit(TaskPolicy, :index?)
-  step :params!
-  fail :params_error!
   step :model!
 
-  def params!(_options, params:, **)
-    params[:project_id].present? && params[:project_id].to_i.positive?
-  end
-
-  def params_error!(options, **)
-    options[:errors] = { project_id: I18n.t('errors.invalid_project_id') }
-  end
-
   def model!(options, params:, current_user:, **)
-    options[:model] = Pundit.policy_scope(current_user, Task).where(project_id: params[:project_id])
+    options[:model] = Pundit.policy_scope(current_user, Project.find_by!(id: params[:project_id]).tasks)
   end
 end
