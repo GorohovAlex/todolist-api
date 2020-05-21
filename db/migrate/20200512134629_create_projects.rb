64ac6c60979bbad09ec4ff2b200cec1181985aa0
GorohovAlex <gorohov.as@gmail.com>
2020-05-21 11:36:21 +0300

    Change migrate create projects

--- db/migrate/20200512134629_create_projects.rb
+++ db/migrate/20200512134629_create_projects.rb
@@ -1,8 +1,8 @@
 class CreateProjects < ActiveRecord::Migration[6.0]
   def change
     create_table :projects do |t|
-      t.string :name
-      t.references :user, foreign_key: { on_delete: :cascade }
+      t.string :name, null: false
+      t.references :user, null: false, foreign_key: { on_delete: :cascade }
 
       t.timestamps
     end
