032d4612220ea0346a60c955e692079086239163
GorohovAlex <gorohov.as@gmail.com>
2020-05-21 14:54:43 +0300

    Add dependent

--- db/migrate/20200515203023_create_task_comments.rb
+++ db/migrate/20200515203023_create_task_comments.rb
@@ -2,7 +2,7 @@ class CreateTaskComments < ActiveRecord::Migration[6.0]
   def change
     create_table :task_comments do |t|
       t.references :task, foreign_key: { on_delete: :cascade }
-      t.string :comment
+      t.string :comment, null: false
       t.string :image
       t.timestamps
     end
