31a4e58e6bd59325f0383a028275e56a0b77b95c
GorohovAlex <gorohov.as@gmail.com>
2020-05-21 15:19:58 +0300

    Refactoring TaskComment index

--- app/concepts/task_comment/operation/index.rb
+++ app/concepts/task_comment/operation/index.rb
@@ -1,18 +1,7 @@
 class TaskComment::Operation::Index < Trailblazer::Operation
-  step Policy::Pundit(TaskPolicy, :index?)
-  step :params!
-  fail :params_error!
   step :model!
-
-  def params!(_options, params:, **)
-    params[:task_id].present? && params[:task_id].to_i.positive?
-  end
-
-  def params_error!(options, **)
-    options[:errors] = { project_id: I18n.t('errors.invalid_task_id') }
-  end
-
+  step Policy::Pundit(TaskPolicy, :index?)
   def model!(options, params:, current_user:, **)
-    options[:model] = Pundit.policy_scope(current_user, Task).find_by!(id: params[:task_id]).task_comments
+    options[:model] = Pundit.policy_scope(current_user, Task.find_by!(id: params[:task_id]).task_comments)
   end
 end
