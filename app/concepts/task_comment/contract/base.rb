f0f9415dab3d5d6f85da5912e676f70b05bfdcf2
GorohovAlex <gorohov.as@gmail.com>
2020-05-22 14:33:34 +0300

    Remove validations

--- app/concepts/task_comment/contract/base.rb
+++ app/concepts/task_comment/contract/base.rb
@@ -2,7 +2,4 @@ class TaskComment::Contract::Base < Reform::Form
   property :comment
   property :task_id
   property :image
-
-  validates :comment, presence: true
-  validates :task_id, presence: true, numericality: true
 end
