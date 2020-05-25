98a81e6e1bc27eea335b3012a1eea1feed0e9ede
GorohovAlex <gorohov.as@gmail.com>
2020-05-25 14:10:17 +0300

    Change TaskComment Contract

--- app/concepts/task_comment/contract/create.rb
+++ app/concepts/task_comment/contract/create.rb
@@ -1,4 +1,4 @@
 class TaskComment::Contract::Create < TaskComment::Contract::Base
   validates :comment, presence: true
-  validates :task_id, presence: true, numericality: true
+  validates :task_id, presence: true
 end
